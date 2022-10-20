b5acedb7adfc44c0ffed03435e43d4ea2f11ccca
klaudorowicz <k.laudorowicz@gmail.com>
2022-10-20 08:37:33 +0200

    Done and hide test of probability section; correction of comments; started creation a f() playRound

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-
-// declaration Strings to game
+// Declaration Strings to game
 const rock = "Rock";
 const paper = "Paper";
 const scissors = "Scissors";
@@ -9,7 +8,8 @@ let choices = [rock, paper, scissors];
 console.log(choices);
 console.log(typeof(choices));
 
-// function which choose random element 
+
+// Function which choose random element 
 function random(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -17,33 +17,49 @@ function random(min, max) {
 }
 
 
-// function which choose random string from array
+// Function which choose random string from array
 function getComputerChoice(choices) {
   return choices[random(0, choices.length)];
 }
 
-// console.log(getComputerChoice(choices));
+
+// Testing section for count probability of drawing
+  /*
+  let testCalcRock = 0;
+  let testCalcPaper = 0;
+  let testCalcScissors = 0;
+  let testFunction = getComputerChoice;
+  let test1;
+
+  function get100() {
+    for(i = 0; i < 1000; ++i) {
+      test1 = testFunction(choices);
+      if (test1 === "Paper") {
+        testCalcPaper += 1;
+      } else if (test1 === "Rock") {
+        testCalcRock += 1;
+      } else {
+        testCalcScissors += 1;
+      }
+    }
+  }
+
+  get100()
+  console.log('Sum = ' + calcRock + ' Rock');
+  console.log('Sum = ' + calcPaper + ' Paper');
+  console.log('Sum = ' + calcScissors + ' Scissors');
+  */
 
 let calcRock = 0;
 let calcPaper = 0;
 let calcScissors = 0;
-let testFunction = getComputerChoice;
-let test1;
 
-function get100() {
-  for(i = 0; i < 1000; ++i) {
-    test1 = testFunction(choices);
-    if (test1 === "Paper") {
-      calcPaper += 1;
-    } else if (test1 === "Rock") {
-      calcRock += 1;
-    } else {
-      calcScissors += 1;
-    }
-  }
+
+// Declaration two parameters to compare (Player vs Comp)
+const computerSelection = getComputerChoice();
+const playerSelection = "Rock";
+
+// Function that plays a single round of Rock Paper Scissors
+function playRound(computerSelection, playerSelection) {
+
 }
-
-get100()
-console.log('Sum = ' + calcRock + ' Rock');
-console.log('Sum = ' + calcPaper + ' Paper');
-console.log('Sum = ' + calcScissors + ' Scissors');
